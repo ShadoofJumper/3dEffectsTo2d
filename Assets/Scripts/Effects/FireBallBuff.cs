e3d8328ae08c93568ce123a31708f5bce22edd34
Ihor Zhurakovskyi <>
2023-04-01 18:55:22 +0300

    Initial commit

---
+++ Assets/Scripts/Effects/FireBallBuff.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class FireBallBuff : MonoBehaviour
+{
+	[Tooltip ("Axis of rotation - use 0 or 1 only")]
+    public Vector3 _rotationAxis = new Vector3 (0,1,0);
+    [Tooltip ("Speed of rotation in degrees per second")]
+    public float _rotationSpeed = 90;
+	
+    private Transform _transform;
+	
+    void Start () {
+	
+        // cache the transform
+        _transform = transform;
+    }
+	
+	
+    void Update ()
+    {
+        _transform.Rotate (_rotationAxis * _rotationSpeed * Time.deltaTime);
+    }
+}
+
+
